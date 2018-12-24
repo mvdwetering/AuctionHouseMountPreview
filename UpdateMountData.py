fde2f1965691e313c8f15ba17a9dfe85f500dad0
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2018-12-24 18:18:36 +0100

    Update to new Blizzard API

--- UpdateMountData.py
+++ UpdateMountData.py
@@ -2,14 +2,13 @@
 # Input data obtained from the Battle.NET API
 #
 # How to get/update the data:
-# * Get an API key from https://dev.battle.net/
-# * Create a file called "apikey" in the same directory as this script and put your Battle.NET API key in there.
+# * Get an API key from https://develop.battle.net/
+# * Create files called "client_id" and "client_secret" in the same directory as this script and put your Battle.NET API Client ID and Client Secret in there.
 # * Run this script, it will update mounts.lua with the latest data from the API
 # * Commit and release
 #
 
-import requests
-import json
+from wowapi import WowApi
 
 def GenerateLuaOutput(name, itemList):
     with open("{0}.lua".format(name), "w") as text_file:
@@ -24,28 +23,34 @@ def GenerateLuaOutput(name, itemList):
 
         for itemId in sorted(itemList.keys()):
             text_file.write( "{0}[{1}] = {{ creatureId = {2}, spellId = {3} }}  -- {4}\n".format(name, itemId, itemList[itemId]["creatureId"], itemList[itemId]["spellId"], itemList[itemId]["name"]))
-            
+
         text_file.write("\n")
         text_file.write("AHMP.{0} = {0}\n".format(name))
         text_file.write("\n")
 
+def GetClientId():
+    return GetFirstLineOfFile("client_id")
 
-def GetApiKey():
-    with open ("apikey", "r") as apikeyfile:
-        apikey = apikeyfile.readline()
-        return apikey
-        
+def GetClientSecret():
+    return GetFirstLineOfFile("client_secret")
 
-# Gather relevant data from json obtained from the Battle.net API
-r = requests.get('https://eu.api.battle.net/wow/mount/?locale=en_GB&apikey={}'.format(GetApiKey()))
-if r.status_code == 200:
-    data = r.json()
+def GetFirstLineOfFile(name):
+    with open (name, "r") as thefile:
+        firstline = thefile.readline()
+        return firstline
+
+def main():
+    api = WowApi(GetClientId(), GetClientSecret())
+    response = api.get_mounts("eu")
 
     mounts = {}
-    for mount in data["mounts"]:
-        # Mounts without itemIds are not learned from items and therefore cannot be on the AH or vendor and can be excluded
-        # for example the Paladin and Warlock mounts
+    for mount in response["mounts"]:
+        # Mounts without itemIds are not learned from items and therefore cannot be on the AH or vendor.
+        # Examples are the Paladin and Warlock mounts
         if mount["itemId"]:
             mounts[mount["itemId"]] = { 'name': mount["name"], 'creatureId': mount["creatureId"] , 'spellId': mount["spellId"] }
 
     GenerateLuaOutput("mounts", mounts)
+
+if __name__ == "__main__":
+    main()
